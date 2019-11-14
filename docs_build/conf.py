0c0e29ee5b87af5e1d2b50da914b6df34591453a
Tres Seaver <tseaver@palladion.com>
2019-11-13 19:27:52 -0500

    fix: update URL for requests docs inventory

--- docs_build/conf.py
+++ docs_build/conf.py
@@ -181,5 +181,5 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3.6', None),
-    'requests': ('http://requests.kennethreitz.org/en/stable', None),
+    'requests': ('https://requests.readthedocs.org/en/latest', None),
 }
