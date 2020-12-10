8cc0dafd1354250eae9fd1f3d3d53654c8fabda7
Tres Seaver <tseaver@palladion.com>
2020-12-10 16:59:40 -0500

    fix: overlooked python-requires change

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setuptools.setup(
     zip_safe=False,
     install_requires=REQUIREMENTS,
     extras_require=EXTRAS_REQUIRE,
-    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
