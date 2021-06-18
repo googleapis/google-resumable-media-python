8a18cede2e86b3a0a69502fe3ae5ee18ac8abb47
Dan Lee <71398022+dandhlee@users.noreply.github.com>
2021-06-18 06:11:34 -0400

    chore: move non-cloud package to staging bucket

--- owlbot.py
+++ owlbot.py
@@ -9,6 +9,10 @@ common = gcp.CommonTemplates()
 templated_files = common.py_library()
 s.move(templated_files / ".kokoro")
 s.move(templated_files / ".trampolinerc")
-s.move(templated_files / "docs", excludes=["multiprocessing.rst", "conf.py"])
+s.move(templated_files / "docs", excludes=[
+  "multiprocessing.rst", 
+  "conf.py",
+  ".kokoro/docs/*"
+])
 
 s.shell.run(["nox", "-s", "blacken"], hide_output=False)
