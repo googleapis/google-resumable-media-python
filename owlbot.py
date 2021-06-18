ef28f77ae57cf85c092da69cad44c46e90dba4fa
Dan Lee <71398022+dandhlee@users.noreply.github.com>
2021-06-18 06:15:59 -0400

    chore: update Owlbot.py

--- owlbot.py
+++ owlbot.py
@@ -7,12 +7,13 @@ common = gcp.CommonTemplates()
 # Add templated .kokoro files
 # ----------------------------------------------------------------------------
 templated_files = common.py_library()
-s.move(templated_files / ".kokoro")
+s.move(templated_files / ".kokoro", excludes=[
+  ".kokoro/docs/*"
+])
 s.move(templated_files / ".trampolinerc")
 s.move(templated_files / "docs", excludes=[
   "multiprocessing.rst", 
-  "conf.py",
-  ".kokoro/docs/*"
+  "conf.py"
 ])
 
 s.shell.run(["nox", "-s", "blacken"], hide_output=False)
