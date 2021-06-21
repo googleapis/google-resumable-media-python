14e56e8b0615b68173dca73f6ce2bf18b6a5cef7
Dan Lee <71398022+dandhlee@users.noreply.github.com>
2021-06-21 15:20:19 -0400

    chore: update Owlbot.py

--- owlbot.py
+++ owlbot.py
@@ -7,13 +7,18 @@ common = gcp.CommonTemplates()
 # Add templated .kokoro files
 # ----------------------------------------------------------------------------
 templated_files = common.py_library()
-s.move(templated_files / ".kokoro", excludes=[
-  "docs/*"
-])
+s.move(templated_files / ".kokoro")
 s.move(templated_files / ".trampolinerc")
 s.move(templated_files / "docs", excludes=[
   "multiprocessing.rst", 
   "conf.py"
 ])
+
+# Block pushing non-cloud libraries to Cloud RAD
+s.replace(
+    ".kokoro/docs/common.cfg",
+    r'value: "docs-staging-v2"',
+    r'value: "docs-staging-v2-staging"'
+)
 
 s.shell.run(["nox", "-s", "blacken"], hide_output=False)
